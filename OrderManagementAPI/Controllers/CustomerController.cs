6e4bc90a71bf738bd7145b020b1045f3e8fc08e4
George Darchiashvili <Gdarchiashvili@hotmail.com>
2026-02-16 16:41:39 +0400

    feat(api): implement Customer CRUD endpoints

---
+++ OrderManagementAPI/Controllers/CustomerController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OrderManagementAPI.Data;
+using OrderManagementAPI.Models;
+
+namespace OrderManagementAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CustomerController : ControllerBase
+    {
+        private readonly OrderManagementDbContext _context;
+
+        public CustomerController(OrderManagementDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll() => Ok(_context.Customers.ToList());
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            return customer == null ? NotFound() : Ok(customer);
+        }
+
+        [HttpPost]
+        public IActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(GetById), new { id = customer.Id }, customer);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, Customer updated)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null) return NotFound();
+
+            customer.Name = updated.Name;
+            customer.Email = updated.Email;
+            customer.Address = updated.Address;
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var customer = _context.Customers.Find(id);
+            if (customer == null) return NotFound();
+
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+            return NoContent();
+        }
+    }
+}

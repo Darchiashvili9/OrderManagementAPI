b685d7022eeaa03a8b55f7dafbb299e13b9db65d
George Darchiashvili <Gdarchiashvili@hotmail.com>
2026-02-16 13:35:25 +0400

    feat(models): add Customer, Product, Order, OrderItem entities

---
+++ OrderManagementAPI/Models/Order.cs
@@ -0,0 +1,13 @@
+﻿namespace OrderManagementAPI.Models
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public int CustomerId { get; set; }
+        public DateTime OrderDate { get; set; } = DateTime.UtcNow;
+        public string Status { get; set; } = "Pending";
+
+        public Customer? Customer { get; set; }
+        public List<OrderItem> Items { get; set; } = new();
+    }
+}

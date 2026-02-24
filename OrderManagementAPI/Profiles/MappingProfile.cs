39c0cfe3ad2072040aa645ed93afd3bd3d1f7e05
George Darchiashvili <Gdarchiashvili@hotmail.com>
2026-02-24 13:03:27 +0400

    chore(mapping): add AutoMapper profiles for DTOs

---
+++ OrderManagementAPI/Profiles/MappingProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using OrderManagementAPI.DTOs;
+using OrderManagementAPI.Models;
+
+namespace OrderManagementAPI.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Customer, CustomerDto>().ReverseMap();
+            CreateMap<Product, ProductDto>().ReverseMap();
+            CreateMap<Order, OrderDto>().ReverseMap();
+            CreateMap<OrderItem, OrderItemDto>().ReverseMap();
+        }
+    }
+}
